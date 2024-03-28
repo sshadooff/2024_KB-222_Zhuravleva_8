aa3ac4021fb42052ffea20d7c33d5b0cd9d3e632
Tatyana <juravleva.tanya01@yandex.ru>
2024-03-28 13:52:37 +0300

    Start Window

--- fitnessinctructor.cpp
+++ fitnessinctructor.cpp
@@ -1,15 +1,21 @@
 #include "fitnessinctructor.h"
 #include "ui_fitnessinctructor.h"
+#include <QPixmap>
 
 FitnessInctructor::FitnessInctructor(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::FitnessInctructor)
 {
     ui->setupUi(this);
+
+    QPixmap pix(":/resources/img/start.jpg");
+    int w = ui->startImg->width();
+    int h = ui->startImg->height();
+
+    ui->startImg->setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
 }
 
 FitnessInctructor::~FitnessInctructor()
 {
     delete ui;
 }
-
